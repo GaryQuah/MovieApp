dab30dace0f2adc2cb6e2878a47abb3e7e0d671f
GaryQuah <garyquah13@gmail.com>
2025-05-27 17:51:28 +0800

    setup MongoDB connection
    - cd movieapp/server npm start

---
+++ Movie_App/MovieApp/server/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/MovieApp");
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
